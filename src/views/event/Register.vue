0c9a598a946d61d809ad9739da9afe5d24cc4457
David Maier <david.maier@schmalz.de>
2022-09-23 07:43:42 +0000

    solved L6

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,9 +1,21 @@
 <template>
     <p>Register for the event here</p>
+    <button @click="register">Register me</button>
 </template>
 
 <script>
 export default {
     props: ['event'],
+    methods: {
+        register() {
+            // Call to API
+            // If registered then redirect to event details
+            this.$router.push({
+                name: 'EventDetails',
+                // Specifying id is obsolete because we redirect to parent component
+                // params: { id: this.event.id },
+            })
+        },
+    },
 }
 </script>
